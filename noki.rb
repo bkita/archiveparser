b8bfcb4deac1804bda082ede46aefaf6946ee115
Bartosz Kita <bartosz@Mindss-MacBook-Pro.local>
2015-04-09 15:36:19 +0100

    Removed unnecessary comments

--- noki.rb
+++ noki.rb
@@ -10,13 +10,9 @@ snapshots_counter = 0
 (start_date..end_date).each do |year|
 	page = Nokogiri::HTML(open("http://web.archive.org/web/#{year}0600000000*/http://#{domain}"))   
 	snapshots = page.css('div.pop')
-	snapshots_counter += snapshots.size
+	ßsnapshots_counter += snapshots.size
 	puts year.to_s + ' - ' + snapshots.size.to_s
 end
-   
-#page = Nokogiri::HTML(open("http://web.archive.org/web/20051200000000*/http://di.pl"))   
-#captures = page.css('div.pop')
-#news_links.each{|link| puts link['href']}
 
 puts snapshots_counter
 
