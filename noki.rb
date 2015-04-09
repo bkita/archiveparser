278dedc34f33be1f213197d962a6c59f73bbdd86
Bartosz Kita <bartosz.kita@gmail.com>
2015-04-09 16:13:10 +0100

    Added total number of snapshots label

--- noki.rb
+++ noki.rb
@@ -3,17 +3,17 @@ require 'nokogiri'
 require 'open-uri'
 
 start_date = 2005
-end_date = 2014
+end_date = 2007
 domain = "bobosklep.pl"
 snapshots_counter = 0
 
 (start_date..end_date).each do |year|
 	page = Nokogiri::HTML(open("http://web.archive.org/web/#{year}0600000000*/http://#{domain}"))   
 	snapshots = page.css('div.pop')
-	ßsnapshots_counter += snapshots.size
+	snapshots_counter += snapshots.size
 	puts year.to_s + ' - ' + snapshots.size.to_s
 end
 
-puts snapshots_counter
+puts "Total number of snapshots: " + snapshots_counter.to_s
 
 
