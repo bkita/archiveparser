14c3867ac4fa9b0bd970b973d6da8af9590e3794
Bartosz Kita <bartosz.kita@gmail.com>
2015-04-13 17:41:45 +0100

    Added first version of WHOIS check to go

--- go.rb
+++ go.rb
@@ -4,9 +4,16 @@ require 'open-uri'
 
 # Package files
 require File.dirname(__FILE__) + '/archive_parser.rb'
+require File.dirname(__FILE__) + '/whois_parser.rb'
 
-Archive.read_domains_from_file
-Archive.archive_data
+#Archive.read_domains_from_file
+#Archive.archive_data
 
 # Domains to check with WHOIS
-Archive.domains_whois
+#domains = Archive.domains_whois
+
+domains = []
+domains << "1kredythipoteczny.pl"
+domains << "1noc.pl"
+
+WhoisParser.whois_details(domains)
