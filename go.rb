2eb1cfc5d253c6628e49a1484a33a0044eb3e63a
Bartosz Kita <bartosz.kita@gmail.com>
2015-04-16 16:03:23 +0100

    Comented out Whois check - just for testing

--- go.rb
+++ go.rb
@@ -9,17 +9,19 @@ require File.dirname(__FILE__) + '/file_reader.rb'
 
 domains = FileReader.read_domains_from_file
 archive = Archive.new
-whois = WhoisParser.new
+#whois = WhoisParser.new
 
-archive_domains = archive.get_archive(domains)
+#get_full_archive
+#archive_domains = 
+archive.get_short_archive(domains)
 
-archive_domains.each do |domain, archive_summary|
-	puts archive_summary
-end
+#archive_domains.each do |domain|
+#	puts domain
+#end
 
-whois_domains = whois.get_whois(archive_domains)
-whois_domains.each do |domain, whois_summary|
-	puts whois_summary
-end
+#whois_domains = whois.get_whois(archive_domains)
+#whois_domains.each do |domain, whois_summary|
+#	puts whois_summary
+#end
 
 
